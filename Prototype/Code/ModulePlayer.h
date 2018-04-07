c3f75d701f09d3aeea88ba0a346a47187c9d412e
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2018-04-07 20:06:36 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/AlexandruC5/Project-1""
    This reverts commit 10608f5908bdb65fb3aff0fa6556e4e368d6f897.

---
+++ Prototype/Code/ModulePlayer.h
@@ -0,0 +1,33 @@
+#ifndef __ModulePlayer_H__
+#define __ModulePlayer_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "p2Point.h"
+
+struct SDL_Texture;
+
+class ModulePlayer : public Module
+{
+public:
+	ModulePlayer();
+	~ModulePlayer();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+
+	int aux;
+	SDL_Texture* graphics = nullptr;
+	Animation* current_animation = nullptr;
+	
+	Animation forward;
+	Animation backward;
+	Animation mid;
+	Animation mid2;
+	iPoint position;
+};
+
+#endif
