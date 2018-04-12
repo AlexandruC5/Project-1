fb7d1a01e39749071ba461f01f36abdf59f39a88
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2018-04-12 21:49:22 +0200

    added tree paralax

--- Prototype/Code/ModuleSceneAir.cpp
+++ Prototype/Code/ModuleSceneAir.cpp
@@ -48,6 +48,10 @@ bool ModuleSceneAir::Start() {
 		textrect[i] = new SDL_Rect();
 	}
 
+	for (int i = 0; i < NUM_LAYERS; i++) {
+		textp[i] = new SDL_Rect();
+	}
+
 
 	
 
@@ -57,39 +61,24 @@ textrect[0]->y = 0;
 textrect[0]->h = 897;	 	
 textrect[0]->w = 5110;
 
-textrect[1]->x = 0;
-textrect[1]->y = 0;
-textrect[1]->h = 16;
-textrect[1]->w = 5510;
+textp[1]->x = 0;
+textp[1]->y = 0;
+textp[1]->h = 500;
+textp[1]->w = 5510;
 
-textrect[2]->x = 0;
-textrect[2]->y = 0;
-textrect[2]->h = 16;
-textrect[2]->x = 5510;
+textp[2]->x = 0;
+textp[2]->y = 0;
+textp[2]->h = 24;
+textp[2]->w = 5510;
+
+textp[3]->x = 0;
+textp[3]->y = 0;
+textp[3]->h = 19;
+textp[3]->w = 5510;
 
 
-	/*
-	
-	textrect[0]->x = 0;
-	textrect[0]->y = 0;
-	textrect[0]->h = 785;
-	textrect[0]->w = 5110;
 
-	textrect[1]->x = 237;
-	textrect[1]->y = 192;
-	textrect[1]->w = 128;
-	textrect[1]->h = 16;
 
-	textrect[2]->x = -21;
-	textrect[2]->y = 192;
-	textrect[2]->w = 128;
-	textrect[2]->h = 16;
-	
-	textrect[3]->x = -56;
-	textrect[3]->y = 192;
-	textrect[3]->w = 128;
-	textrect[3]->h = 16;
-	*/
 	//shipSpawn = App->audio->LoadFx("Assets/Audio/SFX/xmultipl-026.wav");
 	music = App->audio->LoadMusic("assets/audio/music/03_Sky_dance.ogg");
 
@@ -103,21 +92,7 @@ update_status ModuleSceneAir::Update()
 
 
 	//Painting the background
-	App->render->Blit(textures[0], 0, -670, textrect[0], 1);
-	App->render->Blit(textures[1], 0, -700, textrect[1], 0.5f);
 
-
-	App->render->Blit(textures[0], 0, -670, textrect[0], 1);
-	
-
-
-	App->render->Blit(textures[1], 0, -700, textrect[1], 0.5f);
-
-
-	
-
-
-	App->render->Blit(textures[2], 0, 40, textrect[2], 0.5f);
 
 
 	//App->render->Blit(textures[1], xLayer, yLayer, textrect[1]);
@@ -164,6 +139,24 @@ update_status ModuleSceneAir::Update()
 
 	if (App->input->keyboard[SDL_SCANCODE_SPACE] == 1) App->fade->FadeToBlack(this, App->scene_start, 2);
 
+	//Woods
+
+	App->render->Blit(textures[0], 0, -700, textrect[0], 0.5f);
+	App->render->Blit(textures[1], 10, -315, textp[1], 1.0f);
+	App->render->Blit(textures[1], 10, -315, textp[1], 1.0f);
+	App->render->Blit(textures[2], 0, -310, textp[1], 1.0f);
+	App->render->Blit(textures[1], 0, -305, textp[1], 1.1f);
+	App->render->Blit(textures[2], 0, -305, textp[1], 1.1f);
+	App->render->Blit(textures[1], 0, -300, textp[1], 1.3f);
+	App->render->Blit(textures[2], 0, -300, textp[1], 1.3f);
+	App->render->Blit(textures[1], 0, -295, textp[1], 1.5f);
+	App->render->Blit(textures[2], 0, -295, textp[1], 1.5f);
+	App->render->Blit(textures[3], 0, 200, textp[2], 1.7f);
+	App->render->Blit(textures[4], 0, 200, textp[3], 1.7f);
+	App->render->Blit(textures[3], 15, 190, textp[2], 1.7f);
+	App->render->Blit(textures[4], 15, 190, textp[3], 1.7f);
+
+
 	return update_status::UPDATE_CONTINUE;
 }
 
@@ -192,12 +185,18 @@ bool ModuleSceneAir::CleanUp()
 bool ModuleSceneAir::loadMapTextures()
 {
 	LOG("Loading background textures");
+
 	//Load all background textures
+
+
+	
 	textures[0] = App->textures->Load("assets/sprites/Scenes/Scene_Air/background2.png");
+
+	//Woods textures
 	textures[1] = App->textures->Load("assets/sprites/Scenes/Scene_Air/treeline1.png");
-
-	textures[2] = App->textures->Load("assets/sprites/Scenes/Scene_Air/treeline2.png");
-
+    textures[2] = App->textures->Load("assets/sprites/Scenes/Scene_Air/treeline2.png");
+	textures[3] = App->textures->Load("assets/sprites/Scenes/Scene_Air/treeline3.png");
+	textures[4] = App->textures->Load("assets/sprites/Scenes/Scene_Air/treeline4.png");
 	
 
 	//textures[1] = App->textures->Load("Assets/Sprites/Stages/Stage1/Background/BG01.png");
