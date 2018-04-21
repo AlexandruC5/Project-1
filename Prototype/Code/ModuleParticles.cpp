73520aa06f7fa540efd34b117a611461f63b6dfd
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2018-04-21 23:19:32 +0200

    Fixed memory liccck

--- Prototype/Code/ModuleParticles.cpp
+++ Prototype/Code/ModuleParticles.cpp
@@ -111,6 +111,7 @@ bool ModuleParticles::CleanUp()
 	LOG("Unloading particles");
 	App->textures->Unload(graphics);
 	App->textures->Unload(graphics1);
+	App->textures->Unload(graphics2);
 
 	for(uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
 	{
