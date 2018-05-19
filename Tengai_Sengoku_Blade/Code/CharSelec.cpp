72ec45e8579da29cc401ba2a4754203334f32fdc
CarlosUPC <wolfsea.yt@gmail.com>
2018-05-19 19:11:14 +0200

    Project updated
    solved all errors

---
+++ Tengai_Sengoku_Blade/Code/CharSelec.cpp
@@ -0,0 +1,350 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleAudio.h"
+#include "ModuleRender.h"
+#include "ModuleInput.h"
+#include "ModuleWelcomeScreen.h"
+#include "ModuleFadeToBlack.h"
+#include "CharSelec.h"
+#include "ModuleKatana.h"
+#include "ModuleKatanaArrow.h"
+#include "ModuleSceneTemple.h"
+
+#include "SDL_mixer/include/SDL_mixer.h"
+#include "SDL/include/SDL.h"
+
+
+ModuleCharSelec:: ModuleCharSelec()
+{
+	sky = { 0,56,322,115 };
+
+	background = { 0,0,322,226 };
+	background2 = { 0,0,322,226 };
+    headkatana = { 43,33,112,128 };
+	katana = { 283,4,165,211 };
+	lettersK = { 203,207,80,32 };
+	ayin = { 16,238,130,199 };
+	headayin = { 337,267,112,122 };
+	lettersA = { 203,419,80,31 };
+	blueline = { 0,464, 336,48 };
+
+	square1 = { 4,94,46,53 };
+	square2 = { 52,93,46,54 };
+	random = { 9,149,26,22 };
+	time = { 6,72,29,9 };
+	n0 = { 45,19,8,11 };
+	n1 = { 55,19,5,11 };
+	n2 = { 55,19,5,11 };
+	n3 = { 72,19,8,11 };
+	n4 = { 82,19,8,11 };
+	n5 = { 92,19,8,11 };
+	n6 = { 102,19,8,11 };
+	n7 = { 112,19,8,11 };
+	n8 = { 122,19,8,11 };
+	n9 = { 132,19,8,11 };
+
+	Kidle.PushBack({ 87, 4, 32, 33 });
+	Kidle.PushBack({ 152, 5, 32, 33 });
+	Kidle.PushBack({ 188, 5, 32, 33 });
+	Kidle.speed = 0.20f;
+
+	Aidle.PushBack({ 29, 1, 33, 33 });
+	Aidle.PushBack({ 63, 1, 33, 33 });
+	Aidle.PushBack({ 101, 1, 33, 33 });
+	Aidle.speed = 0.20f;
+}
+ModuleCharSelec::~ModuleCharSelec() {}
+bool ModuleCharSelec::Init() {
+	left = 0, right = 0, bgmove = 0;
+	rand = 0, clock=9;
+
+	return true;
+}
+bool ModuleCharSelec::Start() {
+	App->render->camera.x = 0;
+	App->render->camera.y = 0;
+	clock = 9;
+	frames = 0;
+	s = 0;
+	reset = false;
+	player1 = false;
+	player2 = false;
+	state = KATANAP1;
+	players = ONEPLAYER;
+	graphics1 = App->textures->Load("assets/sprites/Scenes/CharSelecScene/Background.png");
+	graphics2 = App->textures->Load("assets/sprites/Scenes/CharSelecScene/Characters.png");
+	graphics3 = App->textures->Load("assets/sprites/UI/UI.png");
+	graphics4 = App->textures->Load("assets/sprites/characters/katana/Katana_Spritesheet.png");
+	graphics5 = App->textures->Load("assets/sprites/characters/ayin/Ayin_Spritesheet2.png");
+	graphics6 = App->textures->Load("assets/sprites/Scenes/CharSelecScene/Background2.png");
+	graphics7 = App->textures->Load("assets/sprites/Scenes/CharSelecScene/sky.png");
+	return true;
+
+}
+
+
+
+update_status ModuleCharSelec::Update() {
+	bool press_D = App->input->keyboard[SDL_SCANCODE_D] == KEY_STATE::KEY_DOWN;
+	bool press_A = App->input->keyboard[SDL_SCANCODE_A] == KEY_STATE::KEY_DOWN;
+	bool press_L = App->input->keyboard[SDL_SCANCODE_LEFT] == KEY_STATE::KEY_DOWN;
+	bool press_R = App->input->keyboard[SDL_SCANCODE_RIGHT] == KEY_STATE::KEY_DOWN;
+	App->render->Blit(graphics7, 0, 31, &sky);
+	
+	if (clock == 0) state = OUTOFTIME;
+	if (App->input->keyboard[SDL_SCANCODE_2] == KEY_STATE::KEY_DOWN) {
+		players = TWOPLAYERS;
+		state = KATANAAYIN;
+		reset = true;
+	}
+
+	switch (players && state) {
+	case ONEPLAYER:
+
+		if (press_D&& state == KATANAP1) state = AYINP1;
+		else if (press_A && state == KATANAP1) state = RANDOMP1;
+		else if (press_A && state == AYINP1) state = KATANAP1;
+		else if (press_D && state == AYINP1) state = RANDOMP1;
+		else if (press_D && state == RANDOMP1) state = KATANAP1;
+		else if (press_A && state == RANDOMP1) state = AYINP1;
+
+		if (App->input->keyboard[SDL_SCANCODE_RETURN] == KEY_STATE::KEY_DOWN) {
+			player1 = true;
+		}
+
+
+
+		App->render->Blit(graphics1, 0, 0, &background);
+		switch (state) {
+		case KATANAP1:
+			//App->render->Blit(graphics3, 23, 157, &square1);
+			App->render->Blit(graphics2, 16, 17, &headkatana);
+			App->render->Blit(graphics2, 135, -3, &katana);
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			App->render->Blit(graphics3, 23, 157, &square1);
+			
+			App->render->Blit(graphics2, 35, 127, &lettersK);
+
+			if (player1) {
+				//App->katana->Enable();
+				App->fade->FadeToBlack(this, App->scene_temple, 2);
+				
+			}
+			break;
+
+		case AYINP1:
+			//App->render->Blit(graphics3, 83, 157, &square1);
+			App->render->Blit(graphics2, 160, -4, &ayin);
+			App->render->Blit(graphics2, 16, 23, &headayin);
+			App->render->Blit(graphics2, 35, 127, &lettersA);
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			App->render->Blit(graphics3, 83, 157, &square1);
+			break;
+		case RANDOMP1:
+			//App->render->Blit(graphics3, 143, 157, &square1);
+			if (rand <= 5) {
+				App->render->Blit(graphics2, 160, -4, &ayin,55);
+				App->render->Blit(graphics2, 16, 23, &headayin,55);
+				App->render->Blit(graphics2, 35, 127, &lettersA);
+				rand+=1.5f;
+			}
+			else if (rand >5 &&rand <= 11) {
+				App->render->Blit(graphics2, 16, 17, &headkatana,55);
+				App->render->Blit(graphics2, 135, -3, &katana,55);
+				App->render->Blit(graphics2, 35, 127, &lettersK);
+				rand+=1.5f;
+			}
+			if (rand > 11) rand = 0;
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			App->render->Blit(graphics3, 143, 157, &square1);
+			break;
+		case OUTOFTIME:
+			App->fade->FadeToBlack(this, App->scene_start, 3);
+			break;
+		}
+	
+		break;
+
+	case TWOPLAYERS:
+		
+		if (press_D && state == KATANAAYIN) state = RANDOMAYIN; //1 player 
+		else if (press_A  && state == KATANAAYIN) state = RANDOMAYIN;
+		else if (press_A  && state == AYINKATANA) state = RANDOMKATANA;
+		else if (press_D  && state == AYINKATANA) state = RANDOMKATANA;
+		else if (press_D && state == AYINRANDOM) state = KATANARANDOM;
+		else if (press_A && state == AYINRANDOM) state = KATANARANDOM;
+		else if (press_D && state == RANDOMKATANA) state = AYINKATANA;
+		else if (press_A && state == RANDOMKATANA) state = AYINKATANA;
+		else if (press_A && state == RANDOMAYIN) state = KATANAAYIN;
+		else if (press_D && state == RANDOMAYIN) state = KATANAAYIN;
+		else if (press_D&& state == KATANARANDOM) state = AYINRANDOM;
+		else if (press_A&& state == KATANARANDOM) state = AYINRANDOM;
+
+		if (press_R && state == KATANAAYIN) state = KATANARANDOM; //2 player
+		else if (press_L && state == KATANAAYIN) state = KATANARANDOM;
+		else if (press_L && state == AYINKATANA) state = AYINRANDOM;
+		else if (press_R && state == AYINKATANA) state = AYINRANDOM;
+		else if (press_R&& state == KATANARANDOM) state = KATANAAYIN;
+		else if (press_L&& state == KATANARANDOM) state = KATANAAYIN;
+
+		else if (press_R && state == AYINRANDOM) state = AYINKATANA;
+		else if (press_L && state == AYINRANDOM) state = AYINKATANA;
+
+		else if (press_R && state == RANDOMKATANA) state = RANDOMAYIN;
+		else if (press_L && state == RANDOMKATANA) state = RANDOMAYIN;
+		else if (press_R && state == RANDOMAYIN) state = RANDOMKATANA;
+		else if (press_L && state == RANDOMAYIN) state = RANDOMKATANA;
+		App->render->Blit(graphics6, 0, 0, &background2);
+
+		switch (state) {
+		case KATANAAYIN:
+			App->render->Blit(graphics2, 145, -3, &katana);
+			//App->render->Blit(graphics3, 23, 157, &square1);
+			App->render->Blit(graphics2, 10, -4, &ayin);
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			
+			App->render->Blit(graphics3, 23, 157, &square1);
+			App->render->Blit(graphics3, 83, 157, &square2);
+			break;
+		case KATANARANDOM:
+			App->render->Blit(graphics2, 145, -3, &katana);
+
+			if (rand <= 5) {
+				App->render->Blit(graphics2, 10, -4, &ayin);
+				rand += 1.5f;
+			}
+			else if (rand >5 && rand <= 11) {
+				App->render->Blit(graphics2, -15, -3, &katana, 55);
+				rand += 1.5f;
+			}
+			if (rand > 11) rand = 0;
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			App->render->Blit(graphics3, 23, 157, &square1);
+			App->render->Blit(graphics3, 143, 157, &square2);
+			break;
+		case AYINKATANA:
+
+			App->render->Blit(graphics2, 160, -4, &ayin);
+			App->render->Blit(graphics2, -15, -3, &katana, 55);
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			App->render->Blit(graphics3, 83, 157, &square1);
+			App->render->Blit(graphics3, 23, 157, &square2);
+			break;
+		case AYINRANDOM:
+			App->render->Blit(graphics2, 160, -4, &ayin);
+			
+			if (rand <= 5) {
+				App->render->Blit(graphics2, 10, -4, &ayin);
+				rand += 1.5f;
+			}
+			else if (rand >5 && rand <= 11) {
+				App->render->Blit(graphics2, -15, -3, &katana, 55);
+				rand += 1.5f;
+			}
+			if (rand > 11) rand = 0;
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			App->render->Blit(graphics3, 83, 157, &square1);
+			App->render->Blit(graphics3, 143, 157, &square2);
+
+
+			break;
+		case RANDOMKATANA:
+			
+			App->render->Blit(graphics2, -15, -3, &katana, 55);
+
+			if (rand <= 5) {
+				App->render->Blit(graphics2, 160, -4, &ayin, 55);
+				rand += 1.5f;
+			}
+			else if (rand >5 && rand <= 11) {
+				App->render->Blit(graphics2, 145, -3, &katana, 55);
+				rand += 1.5f;
+			}
+			if (rand > 11) rand = 0;
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			App->render->Blit(graphics3, 143, 157, &square1);
+			App->render->Blit(graphics3, 23, 157, &square2);
+			break;
+			
+		case RANDOMAYIN:
+			
+			App->render->Blit(graphics2, 10, -4, &ayin);
+
+			if (rand <= 5) {
+				App->render->Blit(graphics2, 160, -4, &ayin, 55);
+				rand += 1.5f;
+			}
+			else if (rand >5 && rand <= 11) {
+				App->render->Blit(graphics2, 145, -3, &katana, 55);
+				rand += 1.5f;
+			}
+			if (rand > 11) rand = 0;
+			App->render->Blit(graphics2, 0, 161, &blueline);
+			App->render->Blit(graphics3, 143, 157, &square1);
+			App->render->Blit(graphics3, 83, 157, &square2);
+			break;
+
+		case OUTOFTIME:
+			App->fade->FadeToBlack(this, App->scene_start, 3);
+			break;
+		}
+		break;
+	}
+	//App->render->Blit(graphics2, 0, 161, &blueline);
+	App->render->Blit(graphics3, 135, 213, &time);
+	App->render->Blit(graphics4, 30, 167, &(Kidle.GetCurrentFrame()));
+	App->render->Blit(graphics5, 90, 167, &(Aidle.GetCurrentFrame()));
+	App->render->Blit(graphics3, 153, 170, &random);
+	frames++;
+	//if (s == 1 && state == TWOPLAYERS) frames = 0, s = 0;
+	if (state == TWOPLAYERS && reset == true) {
+		s = 0;
+		
+	}
+
+	if (frames <= 50) App->render->Blit(graphics3, 166, 211, &n9), clock = 9;
+	else if(frames>=57&&frames<=100)App->render->Blit(graphics3, 166, 211, &n8), clock = 8;
+	else if (frames > 107 && frames <= 200)App->render->Blit(graphics3, 166, 211, &n7), clock = 7;
+	else if (frames >= 207 && frames <= 300)App->render->Blit(graphics3, 166, 211, &n6), clock = 6;
+	else if (frames >= 307 && frames <= 400)App->render->Blit(graphics3, 166, 211, &n5), clock = 5;
+	else if (frames >= 407 && frames <= 500)App->render->Blit(graphics3, 166, 211, &n4), clock = 4;
+	else if (frames >= 507 && frames <= 600)App->render->Blit(graphics3, 166, 211, &n3), clock = 3;
+	else if (frames >= 607 && frames <= 710)App->render->Blit(graphics3, 166, 211, &n2), clock = 2;
+	else if (frames >= 707 && frames <= 800)App->render->Blit(graphics3, 166, 211, &n1), clock = 1;
+	else if (frames >= 807 && frames <= 900)App->render->Blit(graphics3, 166, 211, &n0), clock = 0;
+	//else if (players == TWOPLAYERS && s == 0) frames = 0;
+	//s = 1;
+	else if (players == TWOPLAYERS && s == 0) frames = 0, s = 1;
+	reset = false;
+
+	return update_status:: UPDATE_CONTINUE;
+}
+
+
+
+void ModuleCharSelec::CheckState() {
+	
+
+}
+
+
+bool ModuleCharSelec::CleanUp() {
+	App->textures->Unload(graphics1);
+	App->textures->Unload(graphics2);
+	App->textures->Unload(graphics2);
+	App->textures->Unload(graphics3);
+	App->textures->Unload(graphics4);
+	App->textures->Unload(graphics5);
+	App->textures->Unload(graphics6);
+	App->textures->Unload(graphics7);
+
+	graphics1 = nullptr;
+	graphics2 = nullptr;
+	graphics3 = nullptr;
+	graphics4 = nullptr;
+	graphics5 = nullptr;
+	graphics6 = nullptr;
+	graphics7 = nullptr;
+	return true;
+}
---
+++ Tengai_Sengoku_Blade/Code/ModuleEnemies.cpp
@@ -0,0 +1,308 @@
+#include "Application.h"
+#include "ModuleInput.h"
+#include "ModuleRender.h"
+#include "ModuleEnemies.h"
+#include "ModuleParticles.h"
+#include "ModuleTextures.h"
+#include "Enemy.h"
+#include "GREEN.h"
+#include "Green2.h"
+#include "red.h"
+#include "DEMONPEGTOP.h"
+#include "DEMONWHEEL.h"
+#include "ModulePlayer.h"
+#include "ModulePlayer2.h"
+#include "OrientalGenius.h"
+#include "ModuleSceneTemple.h"
+#include "ModuleKatana.h"
+
+#include "time.h"
+#include "stdlib.h"
+
+#include "SDL\include\SDL_timer.h"
+
+
+#define SPAWN_MARGIN 50
+#define SCREEN_SIZE2 3
+
+ModuleEnemies::ModuleEnemies()
+{
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+		enemies[i] = nullptr;
+}
+
+// Destructor
+ModuleEnemies::~ModuleEnemies()
+{
+}
+
+bool ModuleEnemies::Start()
+{
+	// Create a prototype for each enemy available so we can copy them around
+	
+	//sprites = App->textures->Load("assets/enemies2.png");
+	sprites = App -> textures->Load("assets/enemiestemple.png");
+
+
+	
+	return true;
+}
+
+update_status ModuleEnemies::PreUpdate()
+{
+	// check camera position to decide what to spawn
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (queue[i].type != ENEMY_TYPES::NO_TYPE)
+		{
+			if (queue[i].x * SCREEN_SIZE > App->render->camera.x + (App->render->camera.w * SCREEN_SIZE) + SPAWN_MARGIN)
+			{
+				SpawnEnemy(queue[i]);
+				queue[i].type = ENEMY_TYPES::NO_TYPE;
+				LOG("Spawning enemy at %d", queue[i].x * SCREEN_SIZE);
+			}
+		}
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+// Called before render is available
+update_status ModuleEnemies::Update()
+{
+	for (uint i = 0; i < MAX_ENEMIES; ++i) {
+		if (enemies[i] != nullptr)
+		{
+			enemies[i]->Move();
+			
+		}
+	}
+	
+	for (uint i = 0; i < MAX_ENEMIES; ++i) {
+		if (enemies[i] != nullptr)  {
+			enemies[i]->Draw(sprites);
+			
+			
+		}
+	}
+
+
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleEnemies::PostUpdate()
+{
+	// check camera position to decide what to spawn
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (enemies[i] != nullptr)
+		{
+			if (enemies[i]->position.x * SCREEN_SIZE < (App->render->camera.x) - SPAWN_MARGIN)
+			{
+				LOG("DeSpawning enemy at %d", enemies[i]->position.x * SCREEN_SIZE);
+				delete enemies[i];
+				enemies[i] = nullptr;
+			}
+		}
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+// Called before quitting
+bool ModuleEnemies::CleanUp()
+{
+	LOG("Freeing all enemies");
+
+	App->textures->Unload(sprites);
+
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (enemies[i] != nullptr)
+		{
+			queue[i].type = ENEMY_TYPES::NO_TYPE;
+			delete enemies[i];
+			enemies[i] = nullptr;
+		}
+	}
+
+	return true;
+}
+
+bool ModuleEnemies::AddEnemy(ENEMY_TYPES type, int x, int y)
+{
+	bool ret = false;
+
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (queue[i].type == ENEMY_TYPES::NO_TYPE)
+		{
+			queue[i].type = type;
+			queue[i].x = x;
+			queue[i].y = y;
+			ret = true;
+			break;
+		}
+	}
+
+	return ret;
+}
+
+void ModuleEnemies::SpawnEnemy(const EnemyInfo& info)
+{
+	// find room for the new enemy
+	uint i = 0;
+	for (; enemies[i] != nullptr && i < MAX_ENEMIES; ++i);
+
+	if (i != MAX_ENEMIES)
+	{
+		switch (info.type)
+		{
+			case ENEMY_TYPES::FISH:
+			enemies[i] = new Fish(info.x, info.y);
+			break;
+
+			case ENEMY_TYPES::Green:
+				enemies[i] = new GREEN(info.x, info.y);
+				break;
+			case ENEMY_TYPES::Green2:
+				enemies[i] = new GREEN2(info.x, info.y);
+				break;
+			case ENEMY_TYPES::RED:
+				enemies[i] = new red(info.x, info.y);
+
+				break;
+
+			case ENEMY_TYPES::ORIENTAL_GENIUS:
+				enemies[i] = new OrientalGenius(info.x, info.y);
+
+				break;
+
+			case ENEMY_TYPES::DemonPegTop:
+				enemies[i] = new DEMONPEGTOP(info.x, info.y);
+
+				break;
+			case ENEMY_TYPES::DemonWheel:
+				enemies[i] = new DEMONWHEEL(info.x, info.y);
+
+				break;
+		}
+
+	}
+}
+
+void ModuleEnemies::OnCollision(Collider* c1, Collider* c2)
+{
+	
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (enemies[i] != nullptr && enemies[i]->GetCollider() == c1)
+		{
+
+			//Player collides w enemies
+			if ((c2->type == COLLIDER_TYPE::COLLIDER_PLAYER || c2->type == COLLIDER_TYPE::COLLIDER_HITBOX_AYIN) && c1->type == COLLIDER_TYPE::COLLIDER_ENEMY_PEGTOP) {
+				LOG("collider on");
+				//Katana colliders with enemy
+				if (c2 == App->katana->coll) {
+					if (timer) {
+						time_on_entry = SDL_GetTicks();
+						timer = false;
+					}
+					current_time = SDL_GetTicks() - time_on_entry;
+					if (current_time > 600) {
+						if (App->katana->power_up > 0) {
+							/*
+							App->particles->power_down.speed.x = speed;
+							App->particles->power_down.speed.y = -2;
+							App->particles->AddParticle(App->particles->power_down, App->player->position.x, App->player->position.y);
+							*/
+						}
+						App->katana->power_up--;
+						timer = true;
+					}
+					//App->audio->PlaySoundEffects(App->player->k_power_down);
+					App->katana->spin_pos = true;
+					App->katana->state = SPIN;
+				}
+			}
+
+
+
+			//Kill enemy demon pegtop
+
+			if (c1->type == COLLIDER_TYPE::COLLIDER_ENEMY_PEGTOP && (c2->type == COLLIDER_TYPE::COLLIDER_PLAYER_KATANA_SHOT || c2->type == COLLIDER_TYPE::COLLIDER_PLAYER_AYIN_SHOT)) {
+				LOG("FIRE");
+				num_pegtop++;
+
+				//App->audio->PlaySoundEffects(fx_death);
+				//App->particles->AddParticle(App->particles->explosion, enemies[i]->position.x, enemies[i]->position.y);
+
+				if (num_pegtop >= 4) {
+					//AddEnemy(ENEMY_TYPES::COIN, enemies[i]->position.x, enemies[i]->position.y);
+					num_pegtop = 0;
+				}
+				delete enemies[i];
+				enemies[i] = nullptr;
+
+				if (c2->type == COLLIDER_TYPE::COLLIDER_PLAYER_KATANA_SHOT)
+				{
+					//App->ui->score_koyori += 200;
+				}
+				if (c2->type == COLLIDER_TYPE::COLLIDER_PLAYER_AYIN_SHOT)
+				{
+					//App->ui->score_sho += 200;
+				}
+
+
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			//if (c1->type == COLLIDER_TYPE::COLLIDER_ENEMY && c2->type == COLLIDER_TYPE::COLLIDER_PLAYER_SHOT) {
+
+
+			//	App->particles->AddParticle(App->particles->waterExplosion, enemies[i]->position.x, enemies[i]->position.y, COLLIDER_NONE);
+			//	enemies[i]->OnCollision(c2);
+			//	App->player->score += 200;
+			//	App->player2->score += 200;
+
+			//	delete enemies[i];
+			//	enemies[i] = nullptr;
+			//	break;
+			//}
+			//
+
+			//if (c2->type == COLLIDER_TYPE::COLLIDER_SHOOT && c1->type == COLLIDER_TYPE::COLLIDER_ENEMY) {
+			//	//App->particles->AddParticle(App->particles->enemyattack, enemies[i]->position.x, enemies[i]->position.y, COLLIDER_ENEMY_SHOT);
+			//	if (shoot) {
+			//		App->particles->AddParticle(App->particles->enemyattack, enemies[i]->position.x, enemies[i]->position.y, COLLIDER_ENEMY_SHOT);
+			//		shoot = false;
+			//	}
+			//}
+			
+		}
+		
+
+	}
+	
+}
